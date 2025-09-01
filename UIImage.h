a4c1cbe1b0fe9754fa706402d15470d853f83b8c
fujita8610 <haruto.2004.5.6@gmail.com>
2025-09-01 12:11:18 +0900

    Add files via upload

---
+++ UIImage.h
@@ -0,0 +1,53 @@
+#pragma once
+
+// UIImageの種類
+enum UIImageType
+{
+	// Playシーンで使う
+	UI_IMAGE_PLAY_FOOTER_FRAME,
+	UI_IMAGE_HEART,
+	UI_IMAGE_TARGET_KAPU,
+	UI_IMAGE_SLOT_FRAME,
+	UI_IMAGE_SLOT_REEL,
+	
+	UI_IMAGE_SLOT_CHANCE,
+	UI_IMAGE_SLOT_BELL,
+	
+	UI_IMAGE_SLOT_CHERRY,
+	UI_IMAGE_SLOT_BAR,
+	UI_IMAGE_SLOT_7,
+
+	UI_IMAGE_TARGET_BOSS,
+	
+
+	UI_IMAGE_medal,
+	
+
+	// 種類最大数
+	UI_IMAGE_TYPE_MAX,
+};
+
+// UIImageのデータ
+struct UIImageData
+{
+	bool active;
+	int handle;
+	float posX;
+	float posY;
+};
+
+// 各ロジックごとの関数
+void InitUIImage();
+void LoadUIImage();
+void StepUIImage();
+void UpdateUIImage();
+void DrawUIImage();
+void FinUIImage();
+
+// UIImageを生成する
+void CreateUIImage(UIImageType type,
+	float posX, float posY);
+
+// 表示中のUIImageを未使用にする
+// シーンの切り替え時などで使う
+void ResetUIImage();
